ba289df1746cea35f99f316ca22d52bed2ffd087
prishajain-23 <pj852@nyu.edu>
2025-02-11 09:53:54 -0500

    rotating a flower made from Bezier curves

---
+++ Week3Assignment/Week3Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week3Assignment
+//
+//  Created by Prisha Jain on 2/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week3Assignment/Week3Assignment/Week3AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week3AssignmentApp.swift
+//  Week3Assignment
+//
+//  Created by Prisha Jain on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Week3AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
